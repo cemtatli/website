9c1219c03d2be8ac33e51e4c25bd3c87d5028eb5
Cem Tatlı <cemtatli3535@gmail.com>
2023-01-26 20:24:37 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "class",
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
