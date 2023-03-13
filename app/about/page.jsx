ae1cbbd6e0443a3362c854ef44d7857598b9db02
Cem <cemtatli3535@gmail.com>
2023-03-13 15:22:13 +0300

    feat: added loader

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ArrowRight } from "iconsax-react";
+import Link from "next/link";
 
 export default function About() {
   return (
@@ -22,13 +24,58 @@ export default function About() {
           </p>
         </div>
         <div className="flex gap-2 flex-col w-full">
-          <h3 className="text-base font-medium text-white">Education</h3>
+          <h3 className="text-sm font-medium text-white">Education</h3>
           <p className="text-sm leading-normal">
             {" "}
             <span className="font-semibold italic"> Management Information Systems,</span> Izmir
             Demokrasi University <br />
             <span className="text-sm">2018 – 2022</span>
           </p>
+        </div>
+        <div className="flex gap-2 flex-col w-full">
+          <h3 className="text-sm font-medium text-white">Certificates</h3>
+          <Link
+            href={
+              "https://gelecegiyazanlar.turkcell.com.tr/certificate/dWlkMjA5MzE0Y2lkNTcxcWlkMTExZW5k"
+            }
+            target="_blank"
+            className="text-sm leading-normal group flex items-center hover:underline cursor-pointer hover:text-white transition-colors"
+          >
+            Turkcell Web Development
+            <ArrowRight
+              color="#d9e3f0"
+              size={14}
+              variant="Outline"
+              className="-rotate-45 ml-0.5 hidden group-hover:block"
+            />
+          </Link>
+          <Link
+            className="text-sm leading-normal group flex items-center hover:underline cursor-pointer hover:text-white transition-colors"
+            href={"https://verified.sertifier.com/tr/verify/14416973231229?ref"}
+            target="_blank"
+          >
+            Dijital Okuryazarlık
+            <ArrowRight
+              color="#d9e3f0"
+              size={14}
+              variant="Outline"
+              className="-rotate-45 ml-0.5 hidden group-hover:block"
+            />
+          </Link>
+          <Link
+            className="text-sm leading-normal group flex items-center hover:underline cursor-pointer hover:text-white transition-colors"
+            href={"https://app.patika.dev/certificates/036259d1"}
+            target="_blank"
+          >
+            {" "}
+            Frontend Web Development Patikası{" "}
+            <ArrowRight
+              color="#d9e3f0"
+              size={14}
+              variant="Outline"
+              className="-rotate-45 ml-0.5 hidden group-hover:block"
+            />
+          </Link>
         </div>
       </div>
     </>
--- app/loading.js
+++ app/loading.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div class="loader">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
+    <div className="loader-wrapper">
+      <div className="loader">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </div>
   );
 }
