2241e8aab7d300e7405897cc1efe717ab1ba8466
cemtatli <cemtatli3535@gmail.com>
2023-07-20 23:32:23 +0300

    update

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -4,8 +4,8 @@ const CV = () => {
   return (
     <section className="h-screen-fit w-full">
       <iframe
-        src="https://drive.google.com/file/d/1A3U3J87SQ_BM-dNQrFM2QObEFj8jDuSp"
         title="CV"
+        src="https://drive.google.com/file/d/1A3U3J87SQ_BM-dNQrFM2QObEFj8jDuSp/view?usp=sharing"
         className="h-full w-full"
       />
     </section>
