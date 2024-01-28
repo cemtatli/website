6be5d78564e993a5ea90c58dba1bfe1cf1419758
Cem <cemtatli3535@gmail.com>
2024-01-28 22:07:29 +0300

    Remove unused ESLint configuration and update package dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ["plugin:tailwindcss/recommended"],
+  "next/core-web-vitals": "on",
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.js"],
+      parser: "@typescript-eslint/parser"
+    }
+  ]
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,30 @@ import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import Wrapper from "@/components/layout/wrapper";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from '@vercel/analytics/react';
-import { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/react";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'cemtatli.dev',
-  description: "cemtatli.dev - Cem Tatlı'nın kişisel websitesi, projelerini, blog ve bookmarklarını içerir.",
-  keywords: ['cemtatli, portfolio, blog, bookmarks, web geliştirme, Next.js, TypeScript, frontend, typscript, tailwindcss'],
-  authors: [{ 'name': 'vaycem' }]
-}
+  title: "cemtatli.dev",
+  description:
+    "cemtatli.dev - Cem Tatlı'nın kişisel websitesi, projelerini, blog ve bookmarklarını içerir.",
+  keywords: [
+    "cemtatli, portfolio, blog, bookmarks, web geliştirme, Next.js, TypeScript, frontend, typscript, tailwindcss"
+  ],
+  authors: [{ name: "iltatmec" }]
+};
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className} >
+      <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
-          <Wrapper>{children}
-            <Analytics />
-          </Wrapper>
+          <Wrapper>{children}</Wrapper>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
